f0e066ebad7980b05b0fdc5a740332211e230350
GhostMiner <66090703+Ghost-Miner@users.noreply.github.com>
2023-08-04 16:18:16 +0200

    Changed the submit score button to be BIGGER

--- thronefall/scripts/boardOptions.js
+++ thronefall/scripts/boardOptions.js
@@ -1,163 +1,175 @@
-let doneLoading = false;
-//
-// LOAD TAVBLE
-//
-/*( async function LoadTable () 
+function ChangeShownTable(tableToShow, sectionName)
+{
+	console.log("ChangeShownTable: " + tableToShow + "; " + sectionName);
+	const LBsections = document.getElementsByClassName("LeaderBoard_section");
+
+	for (let i = 0; i < LBsections.length; i++)
 	{
-		let boardTMLRequest;
-		let boardHTML;
-
-		// Invalid table message
-		//boardTMLRequest = await fetch('./leaderboards/invalid-table-msg.html'); // REtrueve the file
-		//boardHTML        = await boardTMLRequest.text(); // Extract its content
-		//document.querySelector('#leaderBoard').innerHTML = boardHTML; // Insert cintent into the selected div
-			
-		// Neuland socre
-		boardTMLRequest = await fetch('./leaderboards/demo/nf-score.html'); // REtrueve the file
-		boardHTML        = await boardTMLRequest.text(); // Extract its content
-		document.querySelector('#nf-score').innerHTML = boardHTML; // Insert cintent into the selected div
-			
-		// Nordfels socre
-		boardTMLRequest = await fetch('./leaderboards/demo/nl-score.html'); // REtrueve the file
-		boardHTML        = await boardTMLRequest.text(); // Extract its content
-		document.querySelector('#nl-score').innerHTML = boardHTML; // Insert cintent into the selected div
-			
-		// Dursstein socre
-		boardTMLRequest = await fetch('./leaderboards/demo/nf-time.html'); // REtrueve the file
-		boardHTML        = await boardTMLRequest.text(); // Extract its content
-		document.querySelector('#nf-time').innerHTML = boardHTML; // Insert cintent into the selected div
-			
-		// Frostsee socre
-		boardTMLRequest = await fetch('./leaderboards/demo/nl-time.html'); // REtrueve the file
-		boardHTML        = await boardTMLRequest.text(); // Extract its content
-		document.querySelector('#nl-time').innerHTML = boardHTML; // Insert cintent into the selected div
-		
-		doneLoading = true
-
-		ResetPositionNumbers("Nordfels");
-		ResetPositionNumbers("Neuland");
-		ResetPositionNumbers("Nordfels time");
-		ResetPositionNumbers("Neuland time");
-
-		AssignNumbersDemo("Nordfels");
-		AssignNumbersDemo("Neuland");
-		AssignNumbersDemo("Nordfels time");
-		AssignNumbersDemo("Neuland time"); 
-
-		///ChangeShownTable('nf-score', 'nordfels_score');
-		
-		CheckAdressHref();		
-		
-		SetTheme();
-		SetFontStyle();
+		LBsections[i].style.display = "none";
 	}
-	) ();*/
-	
-			function ChangeShownTable (tableToShow, sectionName)
-			{ 
-				console.log("ChangeShownTable: " + tableToShow + "; " + sectionName);
-				const LBsections = document.getElementsByClassName("LeaderBoard_section");
-				
-				for (let i = 0; i < LBsections.length; i++)
-				{
-					LBsections[i].style.display = "none";
-				}
-				
-				let showTable = document.getElementById(tableToShow);				
-				showTable.style.display = "block"; 
 
-				window.location.hash = sectionName;
-			}
+	let showTable = document.getElementById(tableToShow);
+	showTable.style.display = "block";
 
-			function ResetPositionNumbers (tableName)
+	window.location.hash = sectionName;
+}
+
+function ChangeCategory(name)
+{
+	document.getElementById("scoresSection").style.display = "none";
+	document.getElementById("timesSection").style.display = "none";
+	document.getElementById("demoSection").style.display = "none";
+
+	document.getElementById("hs_cat_btn").classList.remove("active");
+	document.getElementById("sr_cat_btn").classList.remove("active");
+	document.getElementById("demo_cat_btn").classList.remove("active");
+
+	switch (name)
+	{
+		default:
+			console.log("[ ERROR ] In ChangeCategory(): " + name + " is not a valid category name!");
+			break;
+
+		case "score":
+			document.getElementById("scoresSection").style.display = "block";
+			document.getElementById("hs_cat_btn").classList.add("active");
+
+			break;
+
+		case "time":
+			document.getElementById("timesSection").style.display = "block";
+			document.getElementById("sr_cat_btn").classList.add("active");
+			break;
+		case "demo":
+			document.getElementById("demoSection").style.display = "block";
+			document.getElementById("demo_cat_btn").classList.add("active");
+			break;
+	}
+}
+
+function ResetPositionNumbers(tableName)
+{
+	switch (tableName)
+	{
+		case "Nordfels":
+
+			const NFcollection = document.getElementsByClassName("SB_NF_Position");
+
+			for (let i = 0; i < NFcollection.length; i++)
 			{
-				switch(tableName)
-				{
-					case "Nordfels":
-						
-						const NFcollection = document.getElementsByClassName("SB_NF_Position");
-					
-						for (let i = 0; i < NFcollection.length; i++) 
-						{
-							NFcollection[i].textContent = "#";
-							NFcollection[i].style.textAlign = "center";
-						}
-					break;					
-
-					case "Neuland":
-						const NLcollection = document.getElementsByClassName("SB_NL_Position");
-				
-						for (let i = 0; i < NLcollection.length; i++) 
-						{
-							NLcollection[i].textContent = "#";
-							NLcollection[i].style.textAlign = "center";
-						}
-					break;
-
-					case "Nordfels time":
-						const NFTimeCollection = document.getElementsByClassName("TIME_NF_Position");
-				
-						for (let i = 0; i < NFTimeCollection.length; i++) 
-						{
-							NFTimeCollection[i].textContent = "#";
-							NFTimeCollection[i].style.textAlign = "center";
-						}
-					break;
-
-					case "Neuland time": 
-						const NLTimeCollection = document.getElementsByClassName("TIME_NL_Position");
-				
-						for (let i = 0; i < NLTimeCollection.length; i++) 
-						{
-							NLTimeCollection[i].textContent = "#";
-							NLTimeCollection[i].style.textAlign = "center";
-						}
-					break;
-				}
+				NFcollection[i].textContent = "#";
+				NFcollection[i].style.textAlign = "center";
 			}
-			function AssignNumbersDemo (tableName)
+			break;
+
+		case "Neuland":
+			const NLcollection = document.getElementsByClassName("SB_NL_Position");
+
+			for (let i = 0; i < NLcollection.length; i++)
 			{
-				switch(tableName)
-				{
-					case "Nordfels":
-						const NFcollection = document.getElementsByClassName("SB_NF_Position");
-				
-						for (let i = 0; i < NFcollection.length; i++) 
-						{
-							NFcollection[i].textContent = (i + 1);
-							NFcollection[i].style.textAlign = "center";
-						}
-					break;
-
-					case "Neuland":
-						const NLcollection = document.getElementsByClassName("SB_NL_Position");
-				
-						for (let i = 0; i < NLcollection.length; i++) 
-						{
-							NLcollection[i].textContent = (i + 1);
-							NLcollection[i].style.textAlign = "center";
-						}
-					break;
-
-					case "Nordfels time":
-						const NFTimeCollection = document.getElementsByClassName("TIME_NF_Position");
-
-						for (let i = 0; i < NFTimeCollection.length; i++) 
-						{
-							NFTimeCollection[i].textContent = (i + 1);
-							NFTimeCollection[i].style.textAlign = "center";
-						}
-					break;
-
-					case "Neuland time": 
-						const NLTimeCollection = document.getElementsByClassName("TIME_NL_Position");
-
-						for (let i = 0; i < NLTimeCollection.length; i++) 
-						{
-							NLTimeCollection[i].textContent =(i + 1);
-							NLTimeCollection[i].style.textAlign = "center";
-						}
-					break;
-				}
+				NLcollection[i].textContent = "#";
+				NLcollection[i].style.textAlign = "center";
 			}
-			
+			break;
+
+		case "Nordfels time":
+			const NFTimeCollection = document.getElementsByClassName("TIME_NF_Position");
+
+			for (let i = 0; i < NFTimeCollection.length; i++)
+			{
+				NFTimeCollection[i].textContent = "#";
+				NFTimeCollection[i].style.textAlign = "center";
+			}
+			break;
+
+		case "Neuland time":
+			const NLTimeCollection = document.getElementsByClassName("TIME_NL_Position");
+
+			for (let i = 0; i < NLTimeCollection.length; i++)
+			{
+				NLTimeCollection[i].textContent = "#";
+				NLTimeCollection[i].style.textAlign = "center";
+			}
+			break;
+	}
+}
+
+function AssignNumbersDemo(tableName)
+{
+	switch (tableName)
+	{
+		case "Nordfels":
+			const NFcollection = document.getElementsByClassName("SB_NF_Position");
+
+			for (let i = 0; i < NFcollection.length; i++)
+			{
+				NFcollection[i].textContent = (i + 1);
+				NFcollection[i].style.textAlign = "center";
+			}
+			break;
+
+		case "Neuland":
+			const NLcollection = document.getElementsByClassName("SB_NL_Position");
+
+			for (let i = 0; i < NLcollection.length; i++)
+			{
+				NLcollection[i].textContent = (i + 1);
+				NLcollection[i].style.textAlign = "center";
+			}
+			break;
+
+		case "Nordfels time":
+			const NFTimeCollection = document.getElementsByClassName("TIME_NF_Position");
+
+			for (let i = 0; i < NFTimeCollection.length; i++)
+			{
+				NFTimeCollection[i].textContent = (i + 1);
+				NFTimeCollection[i].style.textAlign = "center";
+			}
+			break;
+
+		case "Neuland time":
+			const NLTimeCollection = document.getElementsByClassName("TIME_NL_Position");
+
+			for (let i = 0; i < NLTimeCollection.length; i++)
+			{
+				NLTimeCollection[i].textContent = (i + 1);
+				NLTimeCollection[i].style.textAlign = "center";
+			}
+			break;
+	}
+}
+
+function ChangeActiveCategory (catName)
+{
+	switch (catName)
+	{
+		default:
+			console.log("[ EEROR ]: Category " + catName + " does not exist.");
+		break;
+
+		case "":
+			console.log("[ INFO ]: No category selected. using default.");
+			ChangeCategory("demo");
+			break;
+
+		case "?score":
+			ChangeCategory("score")
+		break;
+
+		case "?time":
+			ChangeCategory("time")
+		break;
+
+		case "?demo":
+			ChangeCategory("demo")
+		break;
+	}
+} 
+function ChangeActiveBoard (boardName)
+{
+	switch(boardName)
+	{
+		
+	}
+}
