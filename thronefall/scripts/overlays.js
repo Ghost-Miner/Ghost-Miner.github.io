bca81634cd07c43844284b417411f25789abc20b
GhostMiner <66090703+Ghost-Miner@users.noreply.github.com>
2023-08-26 13:10:38 +0200

    almost done wth proof windows

--- thronefall/scripts/overlays.js
+++ thronefall/scripts/overlays.js
@@ -1,4 +1,7 @@
-function OpenSubWindow()
+// 
+// SUBMISSION WINDOW
+// 
+function OpenSubWindow() 
 {
 	document.getElementById("newSubmitOverlay").style.display = "block";
 }
@@ -6,10 +9,6 @@ function CloseSubmitWindow ()
 {
 	document.getElementById("newSubmitOverlay").style.display = "none";
 	ShowChoiceButtons();
-}
-function MinimizeSubmitWindow ()
-{
-	document.getElementById("newSubmitOverlay").style.display = "none";
 }
 function HideChoiceButtons ()
 {
@@ -20,4 +19,35 @@ function ShowChoiceButtons ()
 {
 	document.getElementById("chooseSubTypeButtons").style.display = "block";
 	document.getElementById("submitFormDiv").style.display = "none";
+}
+
+// 
+// NEW SCORE/TIME PROOF 
+// 
+function OpenProofWindowImage(picLink) 
+{
+	document.getElementById("newProofWindowMainImage").style.display = "block";
+
+	var imgLink	   = document.getElementById("newProofImage");
+	imgLink.src = picLink;
+}				  
+function CloseProofWindowImage() 
+{
+	document.getElementById("newProofWindowMainImage").style.display = "none";
+					
+	var imgLink    = document.getElementById("newProofImage");
+	imgLink.src = "";
+}
+
+function OpenVideoWindow(link) 
+{
+	document.getElementById("newProofWindowMainVideo").style.display = "block";
+	var videoLink = document.getElementById("newProofVideo");
+	videoLink.src = link;
+}				  
+function CloseVideoWindow() 
+{
+	document.getElementById("newProofWindowMainVideo").style.display = "none";
+	var videoLink = document.getElementById("newProofVideo");
+	videoLink.src = "";
 }
