0668c0ecc25e5411c5e443c0c5d5d0544e0232bf
GhostMiner <66090703+Ghost-Miner@users.noreply.github.com>
2023-09-18 16:34:20 +0200

    added option to sort columns

--- thronefall/scripts/processScores.js
+++ thronefall/scripts/processScores.js
@@ -1,5 +1,7 @@
 // data from entries.csv parsed into an array
     let tableData;
+// Current sort order
+    let sortOrder = "down";
 
 async function GetSubData ()
 {
@@ -281,10 +283,217 @@ function ChangeTableData(level)
         tableRows[i].style.display = "none";
     }
 
+    // Add order utton to table header
+    document.getElementById("th-position")  .addEventListener("click",() => { OrderBoardBy(level.toLowerCase(), "score") }   , false);
+    document.getElementById("th-name")      .addEventListener("click",() => { OrderBoardBy(level.toLowerCase(), "name") }    , false);
+    document.getElementById("th-score")     .addEventListener("click",() => { OrderBoardBy(level.toLowerCase(), "score") }   , false);
+    document.getElementById("th-coins")     .addEventListener("click",() => { OrderBoardBy(level.toLowerCase(), "gold") }    , false);
+    document.getElementById("th-mutNums")   .addEventListener("click",() => { OrderBoardBy(level.toLowerCase(), "mutators") }, false);
+    document.getElementById("th-usedWeapon").addEventListener("click",() => { OrderBoardBy(level.toLowerCase(), "weapon") }  , false);
+    document.getElementById("th-date")      .addEventListener("click",() => { OrderBoardBy(level.toLowerCase(), "date") }    , false);
     // HIDE LOADING ANIMATION
     document.getElementById("LBLoadingIndicator").style.display = "none";
     // SHOW LEADERBOAR SECTION
     document.getElementById("leaderboardContent").style.display = "block";
     // SET LEADERBOARD TITLE
 	document.getElementById("leaderboardNameText").textContent = chosenMap + " high-scores";
+}
+
+
+function OrderBoardBy (map, orderType)
+{
+    // Initialize array to sort
+    let  chosenMapArray = new Array();
+
+    // Get what ma is selected
+    switch (map)
+    {
+        default:
+            console.error("OrderBoardBy | Invalid value for map! " + map);
+        return;
+
+        case "neuland":
+            chosenMapArray = NeulandSubs;
+        break;
+
+        case "nordfels":
+            chosenMapArray = NordfelsSubs;
+        break;
+
+        case "durststein":
+            chosenMapArray = DurststeinSubs;
+        break;
+
+        case "frostsee":
+            chosenMapArray = FrostseeSubs;
+        break;
+    }
+
+    // Replace all "unknown"s in mutator columns with -1 for better sorting.
+    let chosenMut;
+    let chosenMutString;
+    for (let i = 0; i < chosenMapArray.length; i++)
+    {
+        chosenMut       = chosenMapArray[i];
+        chosenMutString = chosenMut[9];
+
+        if (chosenMutString == "unknown")
+        {                       
+            // console.log("i: " + i + ", " + chosenMut[21] + " mut unknown " + chosenMut[i]);
+            chosenMut[9] = -1;
+        }
+    }
+
+    // Selecte the correct sorting value
+    switch (orderType)
+    {
+        default:
+            console.error("OrderBoardBy | Invalid value for orderType! " + orderType);
+        return;
+        
+        case "score":
+            chosenMapArray.sort((a,b) => parseInt(b[5]) - parseInt(a[5]));
+        break;
+        
+        case "gold":
+            chosenMapArray.sort((a,b) => parseInt(b[6]) - parseFloat(a[6]));
+        break;
+        
+        case "mutators":
+            chosenMapArray.sort((a,b) => parseInt(b[9]) - parseInt(a[9]));
+        break;
+
+        case "weapon":
+            chosenMapArray.sort((a,b) => {
+                    const weaponA = a[11].toUpperCase(); //console.log("A: " + nameA);
+                    const weaponB = b[11].toUpperCase(); //console.log("B: " + nameB);
+                    if (weaponA < weaponB) { return -1; }
+                    if (weaponA > weaponB) { return  1; }
+                    // names must be equal 
+                    return 0;    
+                } );
+        break;
+
+        case "name":
+            chosenMapArray.sort((a,b) => {
+                    const nameA = a[2].toUpperCase(); //console.log("A: " + nameA);
+                    const nameB = b[2].toUpperCase(); //console.log("B: " + nameB);
+                    if (nameA < nameB) { return -1; }
+                    if (nameA > nameB) { return  1; }
+                    // names must be equal 
+                    return 0;    
+                } );
+        break;
+
+        case "date":
+                chosenMapArray.sort((a,b) => {
+                    const dateA = a[0]; 
+                    const dateB = b[0]; 
+                    if (dateA > dateB) { return -1; }
+                    if (dateA < dateB) { return  1; }
+                } );
+        break;
+    }
+
+    // Sett all mutator column with -1 back to unknown
+    for (let i = 0; i < chosenMapArray.length; i++)
+    {
+        chosenMut       = chosenMapArray[i];
+        chosenMutString = chosenMut[9];
+
+        if (chosenMutString == "-1")
+        {                       
+            // console.log("i: " + i + ", " + chosenMut[21] + " setting back to unknown " + chosenMut[i]);
+            chosenMut[9] = "unknown";
+        }
+    }
+    // console.log("--- chosenMapArray ordered -----------------------------------")
+    // console.log(chosenMapArray);
+
+    ////////////////////////////////////////////////////////////////
+
+     // CREATE ARRAYS FOR EACH VALUE TYPE
+	let position = new Array();
+	let names = new Array();
+	let score = new Array();
+	let coins = new Array();
+	let date = new Array();
+	let proof = new Array();
+    let mutators = new Array();
+    let subID = new Array();
+    let version = new Array();
+    let usedWeapon = new Array();
+    let subIndex = new Array();
+
+    // ASSIGN EACH VALUE TYPE
+	for (let i = 0; i < chosenMapArray.length; i++)
+	{
+		chosenRow = chosenMapArray[i];;
+
+        position[i] = chosenRow[21];
+		names[i] = chosenRow[2];
+		score[i] = chosenRow[5];
+		coins[i] = chosenRow[6];
+		date[i] = chosenRow[8];
+		proof[i] = chosenRow[7];
+        subID[i] = chosenRow[9];
+        mutators[i] = chosenRow[9];
+        version[i] = chosenRow[10];
+        usedWeapon[i] = chosenRow[11];
+        subIndex[i] = chosenRow[20];
+	}
+
+    // GET EACH TABLE COLUMN
+	const tPosition = document.getElementsByClassName("position");
+	const tNames    = document.getElementsByClassName("name");
+	const tScore    = document.getElementsByClassName("score");
+	const tCoins    = document.getElementsByClassName("coins");
+	const tDate     = document.getElementsByClassName("date");
+    //const tProof    = document.getElementsByClassName("proof");
+    const tMutNums  = document.getElementsByClassName("mutNums");
+    const tVersion  = document.getElementsByClassName("version");
+    const tWeapon   = document.getElementsByClassName("usedWeapon");
+    //const tSubID    = document.getElementsByClassName("subID");
+
+	let tableRows = document.getElementsByClassName("tableRow");
+
+    for (let i = 0; i < tableRows.length; i++)
+    {
+        tableRows[i].style.display = "table-row";
+    }
+
+    // REST TEXT INSIDE CELLS
+	for (let k = 0; k < tableRows.length; k++)
+	{
+		tPosition[k].textContent = "";
+		tNames[k].textContent = "";
+		tScore[k].textContent = "";
+		tCoins[k].textContent = "";
+		tDate[k].textContent = "";
+        tMutNums[k].textContent = "";
+        tVersion[k].textContent = "";
+        tWeapon[k].textContent = "";
+        // tSubID[k].textContent = "";
+	}
+
+    // ASSIGN TEXT INSIDE EACH CELL
+	for (let k = 0; k < chosenMapArray.length; k++)
+	{
+		tPosition[k].textContent = position[k];
+		tNames[k].textContent = names[k];
+
+        tScore[k].textContent = SplitScore(score[k]);
+		tCoins[k].textContent = coins[k];
+        tMutNums[k].textContent = mutators[k];
+		tDate[k].textContent = RemoveTimeFromData(date[k]);
+        tVersion[k].textContent = version[k];
+        tWeapon[k].textContent = usedWeapon[k];
+        
+        tableRows[k].addEventListener("click",() => { ShowSubInfo(subIndex[k]) },false,);
+	}
+    // HIDE ALL EMPTY ROWS
+    for (let i = chosenMapArray.length; i < tableRows.length; i++)
+    {
+        tableRows[i].style.display = "none";
+    }
 }
