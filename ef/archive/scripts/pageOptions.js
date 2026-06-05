2b7100f09e85e521d11cbf5e1c89b16e7f1c9e3d
GhostMiner <66090703+Ghost-Miner@users.noreply.github.com>
2026-06-05 21:46:44 +0200

    fixed images

--- ef/archive/scripts/pageOptions.js
+++ ef/archive/scripts/pageOptions.js
@@ -1,3 +1,54 @@
+function GetUrlSearch ()
+{
+    let search = location.search.split("?")[1];
+    return search;
+}
+function GetUrlHash ()
+{
+    let hash = location.hash.split("#")[1];
+    return hash;
+}
+
+function GetImageIndexFromUrl ()
+{
+    try 
+    {
+        if (GetUrlSearch().includes("pic="))
+        {
+            let urlHash = GetUrlSearch();   
+            let imgNum = urlHash.split("=")[1];
+
+            return imgNum;
+        }
+    }
+    catch { }
+    return null;
+}
+function SetImageIndexUrl (_imageIndex)
+{
+    location.hash = "pic=" + _imageIndex;
+}
+function ClearImageIndexUrl()
+{
+    location.hash = "";
+}
+
+function TryOpenImageViewerFromUrl ()
+{
+    let imgIndex = GetImageIndexFromUrl(); 
+    if (imgIndex != null && imgIndex < imageItemArray.length)
+    {
+        try 
+        {
+            OpenImageView(imageItemArray[imgIndex]);
+        }
+        catch (ex)
+        {
+            console.log(ex);
+        }
+    }
+}
+
 function AddKeyboardEvents ()
 {
     $(document).off("keyup");
@@ -18,14 +69,16 @@ function KeyPressed (e)
                 break;
 
             case "ArrowLeft":
-                try {
+                try 
+                {
                     OpenImageView(imageItemArray[openImageData[IMAGE_INDEX_COL] - 1]);
                 } catch (ex) {};
                 break;
 
             case "ArrowRight":
-                try {
-                OpenImageView(imageItemArray[openImageData[IMAGE_INDEX_COL] + 1])
+                try 
+                {
+                    OpenImageView(imageItemArray[openImageData[IMAGE_INDEX_COL] + 1])
                 } catch (ex) {};
                 break;
         }
